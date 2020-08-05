59bc73c0d3f4e98bf6fc8601e0d912c518be47c0
fonwin <fonwinz@gmail.com>
2020-08-05 08:16:57 +0800

    盤中零股 Fmt23，總是有提供買賣報價，即使檔數為0;

--- f9tws/ExgMdFmt23.hpp
+++ f9tws/ExgMdFmt23.hpp
@@ -23,7 +23,14 @@ struct ExgMdFmt23v1 : public ExgMdFmtRtBase {
    ExgMdOddPriQty    PQs_[1];
 
    bool HasBS() const {
-      return (this->ItemMask_ & 0x7e) != 0;
+      // 盤中零股採用集合競價，每次的 Fmt23 揭示都包含「買賣報價」，
+      // 如果市場上確實沒有買賣，則 (this->ItemMask_ & 0x7e) 為 0;
+      // 所以 Fmt23 的 HasBS() 總是為 true; 表示有提供買賣報價(有可能檔數為 0);
+      // return (this->ItemMask_ & 0x7e) != 0;
+      return true;
+
+      // 格式6，採用逐筆撮合，有額外的旗標，用來標示該次 Fmt6 是否包含買賣報價。
+      // return (this->ItemMask_ & 0x7e) || (this->ItemMask_ & 1) == 0;
    }
 };
 fon9_PACK_POP;
