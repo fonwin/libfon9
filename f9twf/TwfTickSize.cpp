8f10d78339cc22665c9ed8d84e03b2d11ccd7e1a
fonwin <fonwinz@gmail.com>
2022-04-13 15:20:19 +0800

    Add: TwfTickSize;

---
+++ f9twf/TwfTickSize.cpp
@@ -0,0 +1,49 @@
+﻿// \file f9twf/TwfTickSize.cpp
+// \author fonwinz@gmail.com
+#include "f9twf/TwfTickSize.hpp"
+#include "fon9/ConfigLoader.hpp"
+#include "fon9/Log.hpp"
+
+namespace f9twf {
+
+using LvPriStepMap = fon9::SortedVector<ContractId, fon9::fmkt::LvPriStepVect>;
+static LvPriStepMap LvPriStepMap_;
+
+f9twf_API void Load_TwfTickSize(fon9::StrView defaultConfigPath, fon9::StrView cfgFileName) {
+   try {
+      fon9::ConfigLoader cfgld{defaultConfigPath.ToString()};
+      cfgld.LoadFile(cfgFileName);
+      fon9::StrView cfgs{&cfgld.GetCfgStr()};
+      while (!cfgs.empty()) {
+         fon9::StrView cfgln = fon9::StrFetchTrim(cfgs, '\n');
+         if (cfgln.empty())
+            continue;
+         fon9::StrView id = fon9::StrFetchTrim(cfgln, '=');
+         if (id.empty())
+            continue;
+         fon9::fmkt::LvPriStepVect lv;
+         lv.FromStr(cfgln);
+         if (lv.Get())
+            LvPriStepMap_.kfetch(id).second = std::move(lv);
+      }
+   }
+   catch (std::exception& e) {
+      fon9_LOG_ERROR("Load_TwfTickSize:", e.what());
+   }
+}
+
+f9twf_API const fon9::fmkt::LvPriStep* GetLvPriStep(ContractId contractId, f9fmkt_TradingMarket mkt, ExgContractType contractType) {
+   auto ifind = LvPriStepMap_.find(contractId);
+   if (ifind == LvPriStepMap_.end()) {
+      contractId.Clear();
+      contractId.Chars_[0] = static_cast<char>(mkt);
+      contractId.Chars_[1] = '.';
+      contractId.Chars_[2] = static_cast<char>(contractType);
+      ifind = LvPriStepMap_.find(contractId);
+      if (ifind == LvPriStepMap_.end())
+         return nullptr;
+   }
+   return ifind->second.Get();
+}
+
+} // namespaces
---
+++ f9twf/TwfTickSize.hpp
@@ -0,0 +1,17 @@
+﻿// \file f9twf/TwfTickSize.hpp
+// \author fonwinz@gmail.com
+#ifndef __f9twf_TwfTickSize_hpp__
+#define __f9twf_TwfTickSize_hpp__
+#include "fon9/fmkt/FmktTools.hpp"
+#include "f9twf/ExgTypes.hpp"
+#include "f9twf/Config.h"
+
+namespace f9twf {
+
+/// 使用 ConfigLoader 載入 TickSize 設定.
+f9twf_API void Load_TwfTickSize(fon9::StrView defaultConfigPath = "", fon9::StrView cfgFileName = fon9::StrView("TwfTickSize.cfg"));
+
+f9twf_API const fon9::fmkt::LvPriStep* GetLvPriStep(ContractId contractId, f9fmkt_TradingMarket mkt, ExgContractType contractType);
+
+} // namespaces
+#endif//__f9twf_TwfTickSize_hpp__
